f72dbc6f71a427613a865c790111c5c3f3e2260d
Philipp Schmitt <philipp@schmitt.co>
2024-01-04 13:57:41 +0100

    Custom emojis by timewarrior state

--- timetracking.lua
+++ timetracking.lua
@@ -9,36 +9,52 @@ local DEFAULT_VALUE = "N/A"
 
 -- Update default values
 cmdtxt.DEFAULT_SHELL = "zsh"
-cmdtxt.DEFAULT_COMMAND = string.format("timewarrior::today-total --minutes || echo '%s'", DEFAULT_VALUE)
+cmdtxt.DEFAULT_COMMAND = string.format(
+  'timewarrior::is-on && echo -n "on " || echo -n "off "; timewarrior::today-total --minutes || echo "N/A"',
+  DEFAULT_VALUE
+)
 
 local function update_text_source_with_cmd_output()
-	local text = utils.exec_cmd(cmdtxt.COMMAND, cmdtxt.TIMEOUT, cmdtxt.SHELL, cmdtxt.DEBUG)
+  local text =
+    utils.exec_cmd(cmdtxt.COMMAND, cmdtxt.TIMEOUT, cmdtxt.SHELL, cmdtxt.DEBUG)
 
-	if text == nil or text == "" then
-		print(string.format("ERROR: Command returned no output. Defaulting to %s", DEFAULT_VALUE))
-		text = DEFAULT_VALUE
-	end
+  if text == nil or text == "" then
+    print(
+      string.format(
+        "ERROR: Command returned no output. Defaulting to %s",
+        DEFAULT_VALUE
+      )
+    )
+    text = DEFAULT_VALUE
+  end
 
-	local color = "#ffffff"
-	local hours = utils.extract_first_digit(text)
+  local tw_status, tw_text = string.match(text, "(%S+)%s*(.*)")
 
-	local valid = (hours ~= nil)
+  local color = "#ffffff"
+  local hours = utils.extract_first_digit(tw_text)
 
-	if valid then
-		print(string.format("Hours: %d", hours))
+  local valid = (hours ~= nil)
 
-		if hours >= 8 then
-			color = "#ff0000"
-		elseif hours >= 7 then
-			color = "#ff8000"
-		elseif hours >= 6 then
-			color = "#fff000"
-		end
-	end
+  if valid then
+    print(string.format("Hours: %d", hours))
 
-	-- prepend emoji
-	text = string.format("⏱️ %s", text)
-	utils.update_text_source(cmdtxt.TARGET_SOURCE, text, color)
+    if hours >= 8 then
+      color = "#ff0000"
+    elseif hours >= 7 then
+      color = "#ff8000"
+    elseif hours >= 6 then
+      color = "#fff000"
+    end
+  end
+
+  -- prepend emoji
+  local emoji = "⏱️"
+  if string.find(tw_status, "on") then
+  else
+    emoji = "⏸️"
+  end
+  tw_text = string.format("%s %s", emoji, tw_text)
+  utils.update_text_source(cmdtxt.TARGET_SOURCE, tw_text, color)
 end
 
 -- override upstream func
@@ -46,32 +62,32 @@ cmdtxt.update_text_source_with_cmd_output = update_text_source_with_cmd_output
 
 ---@diagnostic disable-next-line lowercase-global
 function script_load(settings)
-	cmdtxt.work(settings)
+  cmdtxt.work(settings)
 end
 
 ---@diagnostic disable-next-line lowercase-global
 function script_update(settings)
-	cmdtxt.work(settings)
+  cmdtxt.work(settings)
 end
 
 ---@diagnostic disable-next-line lowercase-global
 function script_defaults(settings)
-	cmdtxt.script_defaults(settings)
+  cmdtxt.script_defaults(settings)
 end
 
 ---@diagnostic disable-next-line lowercase-global
 function script_description()
-	return DESCRIPTION
+  return DESCRIPTION
 end
 
 ---@diagnostic disable-next-line lowercase-global
 function script_properties()
-	return cmdtxt.script_properties()
+  return cmdtxt.script_properties()
 end
 
 ---@diagnostic disable-next-line lowercase-global
 function script_unload()
-	return cmdtxt.script_unload()
+  return cmdtxt.script_unload()
 end
 
 -- vim set ft=lua et ts=4 sw=4 :
